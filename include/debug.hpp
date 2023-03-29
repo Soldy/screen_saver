53e1abb0e249be26744e036c3fe9cb691dea3130
Soldy <4786022+Soldy@users.noreply.github.com>
2023-03-29 23:12:36 +0100

    debug message extension
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- include/debug.hpp
+++ include/debug.hpp
@@ -2,3 +2,16 @@ void debugMsg(std::string msg, std::string element){
     if(cppConfig::getInt("debug") == 1)
         std::cout << msg << " : " << element << std::endl;
 }
+void debugMsg(
+  std::string msg,
+  std::string element,
+  std::string error
+){
+    if(cppConfig::getInt("debug") == 1)
+        std::cout << 
+            msg << " : " <<
+            element <<
+            " - " <<
+            error <<
+            std::endl;
+}
